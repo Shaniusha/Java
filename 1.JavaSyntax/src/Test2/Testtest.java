231704988d151738b133ccda2aca07c7099c17a1
Shaniusha <151agat15@gmail.com>
2021-05-07 18:50:54 +0300

    fsdfsdfuosufsdf

--- 1.JavaSyntax/src/Test2/Testtest.java
+++ 1.JavaSyntax/src/Test2/Testtest.java
@@ -3,5 +3,16 @@ package Test2;
 public class Testtest {
     public static void main(String[] args) {
         System.out.println("asdasd");
+
+        if (true){
+            System.out.printf("asdasdas");
+        }
+
+        adasdas
+                asdasd
+                asdasd
+                        asdasd
+                        asdasd
+
     }
 }

e9bf883eff1c6ee9af1937466c4a2e9e366fccdb
Shaniusha <151agat15@gmail.com>
2021-05-17 17:34:28 +0300

    16/5

---
+++ 1.JavaSyntax/src/You_Can_Code_Under_Pressure_15_5/You_Can.java
@@ -0,0 +1,12 @@
+package You_Can_Code_Under_Pressure_15_5;
+
+public class You_Can {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i * 2;
+    }
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(doubleInteger(n));
+    }
+}

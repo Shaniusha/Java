cf3fb519acab3193ca0a8f20b87a765685053b6a
Shaniusha <151agat15@gmail.com>
2021-09-18 12:08:17 +0300

    18.09.2021 шабат

---
+++ 1.JavaSyntax/src/Grasshopper_Messi_Goals_18_9/Grasshopper.java
@@ -0,0 +1,27 @@
+package Grasshopper_Messi_Goals_18_9;
+
+public class Grasshopper {
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+
+    public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+    public static void main(String[] args) {
+        System.out.println(totalGoals);
+    }
+}
+//было так
+//public class Grasshopper {
+//    public static int laLigaGoals;
+//    public static int championsLeagueGoals;
+//    public static int copaDelReyGoals;
+//
+//    public static int totalGoals;
+ // закрепить за переменными числа и найти сумму.
+
+//public class GoalsTest {
+//    @Test
+//    public void BasicTests() {
+//        assertEquals(58, Goals.totalGoals);
+//    }
